0a1b2b273142d77f6db5079051b8fefea9f2584b
Maria Viana <mariaviana07@hotmail.com>
2018-11-07 23:18:53 +0000

    clients working

---
+++ cmake-build-debug/CMakeFiles/aeda1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aeda1.dir/main.cpp.o"
+  "CMakeFiles/aeda1.dir/company.cpp.o"
+  "CMakeFiles/aeda1.dir/interfaces.cpp.o"
+  "CMakeFiles/aeda1.dir/people.cpp.o"
+  "CMakeFiles/aeda1.dir/pharmacy.cpp.o"
+  "aeda1.pdb"
+  "aeda1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aeda1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
